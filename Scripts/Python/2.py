a1e804b4bbce390b58a3f6d9ee98d4f6aedd7423
Shahzad Ahmed Khan <shahzad.ahmed1@hbl.com>
2022-06-27 00:43:23 +0500

    Add Script

---
+++ Scripts/Python/2.py
@@ -0,0 +1,31 @@
+import sys
+if 'C:\\MyPythonModules' not in sys.path:
+sys.path.append('C:\\MyPythonModules')
+
+
+# # x = 5
+# # y = 10
+# # x = y
+# # print ("x = ", x)
+# # print ("y = ", y)
+
+# brand = 'Apple'
+# exchangeRate = 1
+# message = 'The price of this %s laptop is %d USD and the exchange rate is %4.2f USD to 1 EUR’ %(brand, 1299, exchangeRate)
+# print (message)
+
+# pets = ['cats', 'dogs', 'rabbits', 'hamsters']
+
+#     for myPets in pets:
+#     print (myPets)
+
+# try:
+#     answer =12/0
+#     print (answer)
+# except Exception as e:
+#     print("ErrorOccur", e)
+
+# import prime
+
+# answer = prime.checkIfPrime(13)
+# print (answer)
---
+++ Scripts/Python/3.py
@@ -0,0 +1,16 @@
+
+
+lst = [1, 2, 3, 4, 5]
+
+print (type(lst))
+print(lst)
+print(lst[2])
+print(lst[2:4])
+
+
+tupl = ("Shahzad","Iqbal","Zubair")
+b = list(tupl)
+b.append("Nawaz")
+tupl = tuple(b)
+print (tupl)
+print (type(tupl))
